77d894c2a879854dfea9222aa52fd3515e615ede
magda <magdamoraga14@gmail.com>
2025-08-09 12:00:13 +0200

    "day 7.2"

---
+++ day7_2.py
@@ -0,0 +1,25 @@
+#Day 7 task 2
+print("Fake Fan Finder")
+favAnime = input("What's your favourite Anime? >")
+if favAnime == "One piece" or favAnime == "One Piece":
+  favCharacter = input("Oh really? Who's your fav character then? >")
+  if favCharacter == "Nami" or favCharacter == "nami":
+    print("Yo good, nami's cool")
+    activitych = input("what is her job? >")
+    if activitych == "maps":
+      print("slayyy")
+  elif favCharacter == "Zoro":
+    print("I love you :3 ")
+    activitych = input("what is he known for? >")
+    if activitych == "forgetting":
+      print("yo good")
+    else:
+      print("get away from me you liar")
+  else:
+    print("Idk what to tell you")
+elif favAnime == "Mo dao Zu shi":
+  print("cool, yeah")
+elif favAnime == "AOT":
+  print("I djfnjfai")
+else:
+    print("No taste no taste")
