898efb8c3546ff17483b42ceef5d5f9ee07f070e
magda <magdamoraga14@gmail.com>
2025-08-09 11:46:12 +0200

    "day6"

---
+++ day6.py
@@ -0,0 +1,9 @@
+#Day 6 - ELIF IF
+print("SECURE LOGIN")
+username = input("Username > ")
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "madzia":
+  print("Hola seniorita madzia, que tal?")
+else:
+ print("Go away!")
