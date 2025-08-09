aa84c0303c81379a023286adb2002b8ad8935812
magda <magdamoraga14@gmail.com>
2025-08-09 10:39:52 +0200

    "Start a 100 days of code replit :3 - day 1"

---
+++ day1.py
@@ -0,0 +1,13 @@
+#100 days of Code in replit!
+
+#Day1:
+print("""Magda
+	August 6th 2025
+	I am signiung up for Replit's 100 days of Python challenge!
+	I will make sure to spend some time every day coding 
+	along, for a minimum of 10 minutes a day.
+	I'll be using Replit, an amazing online IDE so I can do
+	this from my phone wherever I happen to be. 
+	No excuses for not coding from the middle of a field!
+	I am feeling ✨
+You can follow my progres at replit.com/Magdarwwer""")
