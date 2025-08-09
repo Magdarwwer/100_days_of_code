6714620cf226a82ac51a2ea5cee0554bb7c778b5
magda <magdamoraga14@gmail.com>
2025-08-09 10:43:37 +0200

    "day 3"

---
+++ day3.py
@@ -0,0 +1,8 @@
+#Day 3 - practicing print input
+food = input("Name a food> ")
+plant = input("Name a type of plant> ")
+cookin = input("Name a method of cooking> ")
+dFood = input("What word describes burned food? >")
+item = input("Name a DIY item> ")
+print("MENU")
+print(cookin, food," with ", dFood, plant, "on a bed of ", item)
