ca69d3faf16a73628f7c08b6c558c160b2793172
magda <magdamoraga14@gmail.com>
2025-08-09 10:53:54 +0200

    "day 5"

---
+++ day5.py
@@ -0,0 +1,25 @@
+#Day 5
+catsOrDogs = input("Are you a cat or a dog person? Or a dog person? ")
+if catsOrDogs == "cats" or catsOrDogs == "cat":
+   print("Moew :3")
+else:
+  print("hjdsjjsdjh")
+	
+character = input("Which family character are you? ")
+mental = input("Are you a crazy person? ")
+if mental == "Yes" or mental == "yes":
+  print("you are joker ")
+else:
+  print("you are okay :3 good for you")
+	
+kotek = input("Are you a cat? ")
+if kotek == "Yes" or kotek == "yes":
+  print("you are a CAT!meow :3")
+else:
+  print("you are not a meow meow :|")
+
+frog = input("Are you a frog? ")
+if frog == "yes" or frog == "Yes":
+  print("you are a frog :3")
+else:
+    print("you are not a frog")
