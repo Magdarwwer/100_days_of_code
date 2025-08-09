e24ba9a4f13fbc32e6a7a63d4fb5efb7c13734e0
magda <magdamoraga14@gmail.com>
2025-08-09 10:51:00 +0200

    "day 4"

---
+++ day4.py
@@ -0,0 +1,19 @@
+#Day 4 - practicing different colour text
+print("\033[31m""house")
+print("\033[31m""RAINBOW")
+print("\033[33m""RAINBOW")
+print("\033[32m""RAINBOW")
+print("\033[36m""RAINBOW")
+print("\033[34m""RAINBOW")
+print("\033[35m""RAINBOW")
+print("\033[37m""RAINBOW")
+
+print("""
+      "\033[31m"RAINBOW RED"\033[31m"
+      "\033[33m"RAINBOW YELLOW"\033[33m"
+      "\033[32m"RAINBOW GREEN"\033[32m"
+      "\033[36m"RAINBOW CYAN"\033[36m"
+      "\033[34m"RAINBOW BLUE"\033[34m
+      \033[35m RAINBOW PURPLE \033[35m
+      look at "" symbol at purple
+      """)
