50a14fc94e7dfc721c876a0373b8092e8f366900
magda <magdamoraga14@gmail.com>
2025-08-11 11:02:16 +0200

    "day 12"

---
+++ day12.py
@@ -0,0 +1,19 @@
+#Day 12 - debbuging - fixing code 
+print("100 Days of Code QUIZ")
+print("How many can you answer correctly?")
+print()
+ans1 = input("What language are we writing in? >")
+if ans1 == "python":
+  print("Correct")
+else:
+  print("Nope🙈")
+  
+ans2 = int(input("Which lesson number is this?"))
+if(ans2>12):
+  print("We're not quite that far yet")
+elif(ans2==12):
+  print("That's right!")
+else:
+  print("We've gone well past that!")
+
+# fixed print, input, int(), "" and brackets some missing
