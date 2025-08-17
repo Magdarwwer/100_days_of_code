d411115d4afc75b17db0411ccd313b72bda7044f
magda <magdamoraga14@gmail.com>
2025-08-17 21:32:22 +0200

    dictionary note

--- day28.py
+++ day28.py
@@ -1,12 +1,23 @@
-#a += dla listy i stringa oznacza „rozbij string na literki i dodaj każdą literkę osobno do listy”
+#1) a += dla listy i stringa oznacza „rozbij string na literki i dodaj każdą literkę osobno do listy”
 
 # heroes_names = []
 # heroes_names += "Aria"
 # print(heroes_names)  
 # # wynik: ['A', 'r', 'i', 'a']
 
-# To add a whole object to the list 👉 Rozwiązanie: zamiast += użyj .append():
+#2) To add a whole object to the list 👉 Rozwiązanie: zamiast += użyj .append():
 
 # heroes_names.append(character_name)
 # heroes_health.append(character_health)
 # heroes_attack.append(character_strength)
+
+
+#3) Saving hero as a DICTIONARY!!
+# hero = {
+#         "name": character_name,
+#         "type": character_type,
+#         "health": character_health,
+#         "attack": character_strength
+#     }
+    
+#     heroes.append(hero)
