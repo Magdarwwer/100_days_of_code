0ea01ef69b8de38db1844cab0530b26d9f14d1ce
magda <magdamoraga14@gmail.com>
2025-08-17 21:29:31 +0200

    day 28 - figuring out solution without repeating the code, adding to a list in py, "+=" symbol with lists, using tuple

---
+++ day28.py
@@ -0,0 +1,12 @@
+#a += dla listy i stringa oznacza „rozbij string na literki i dodaj każdą literkę osobno do listy”
+
+# heroes_names = []
+# heroes_names += "Aria"
+# print(heroes_names)  
+# # wynik: ['A', 'r', 'i', 'a']
+
+# To add a whole object to the list 👉 Rozwiązanie: zamiast += użyj .append():
+
+# heroes_names.append(character_name)
+# heroes_health.append(character_health)
+# heroes_attack.append(character_strength)
