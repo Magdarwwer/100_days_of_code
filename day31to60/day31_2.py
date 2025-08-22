c829dfbb78ad021b87817c0b7956ebd84245c679
magda <magdamoraga14@gmail.com>
2025-08-22 23:47:18 +0200

    added color changing function

--- day31to60/day31_2.py
+++ day31to60/day31_2.py
@@ -1,2 +1,10 @@
-def colorChange():
+def colorChange(color):
+    if color == " red":
+        return("\033[31m")
+    elif color == "white":
+        return("\033[0m")
+    elif color == "blue":
+        return("\033[34m")
+    elif color == " yellow":
+        return("\033[33m")
     
