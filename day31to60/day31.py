0588ab5064246d1430d0a229fcc06cc7b4397563
magda <magdamoraga14@gmail.com>
2025-08-22 23:16:54 +0200

    day 31 - user inetrface 1 - subrotuine for color changing, text printing

--- day31to60/day31.py
+++ day31to60/day31.py
@@ -1,5 +1,28 @@
 #Day 31 challenge: USER INTERFACE:4\
-def color(color, word):
-  if word == "Honest.":
-    print("red", word) #"get the red code"
-  elif word == "--   ARMBOOK   ---":
+    #use print statements & f-strings
+    #No inputs from the user
+    
+
+#FIRST INTERFACE:
+def changeColor(color, word):
+ if color=="red":
+    print("\033[31m", word, sep="", end="")
+ elif color == "blue":
+   print("\033[0;34m", word, sep="", end="")
+ elif color == "green":
+   print("\033[0;32m", word, sep="", end="")
+ elif color == "brown":
+   print("\033[0;33m", word, sep="", end="")
+ elif color == "purple":
+   print("\033[0;35m", word, sep="", end="")
+ elif color == "cyan":
+    print("\033[0;36m", word, sep="", end="")
+ elif color == "yellow":
+   print("\033[1;33m", word, sep="", end="")   
+ else:
+   print("\033[0m", word, sep="", end="")
+
+
+changeColor("red", "=")
+changeColor("red", "=")
+changeColor("red", "=")
