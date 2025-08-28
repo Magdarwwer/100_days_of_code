81d7d3bf7a3a71454dca84bce2e8e8256a673eed
magda <magdamoraga14@gmail.com>
2025-08-28 23:48:48 +0200

    day 31 - prev next pause texts display modification

--- day31to60/day31.py
+++ day31to60/day31.py
@@ -41,8 +41,10 @@ print(f"{title:^35}")
 # print(f"{changeColor('purple')}{"PURPLE":>35}")
 
 print(f"{changeColor('white', "PREV")}")
+print(f"{changeColor('green', "NEXT")}")
+print(f"{changeColor('white', "PAUSE")}")
 # print(f"{changeColor('green',"NEXT":^35)}")
-print(f"{changeColor('purple')}{"PURPLE":>35}")
+# print(f"{changeColor('purple')}{"PURPLE":>35}")
 
 counter = 0
 
