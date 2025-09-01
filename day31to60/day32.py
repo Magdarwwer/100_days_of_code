11d02693c48ac17b642e98f3c28c22981d4f5761
magda <magdamoraga14@gmail.com>
2025-09-01 23:54:40 +0200

    day 32

--- day31to60/day32.py
+++ day31to60/day32.py
@@ -4,3 +4,18 @@ myList = []
 #index 0
 myList[2] = "stuff"
 #dily remindersa
+timetable = []
+
+timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+print(timetable)
+#tell the computer which index to print out!
+print(timetable[2])
+
+timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+print(timetable[0])
+print(timetable[1])
+print(timetable[2])
+print(timetable[3])
+print(timetable[4])
+
+timetable[4]= "Watch TV"
