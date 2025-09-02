97c4d9474a591ec550e922de7f7db1b12d4a9fa3
magda <magdamoraga14@gmail.com>
2025-09-02 20:51:44 +0200

    day 32 - added common errors exercise

--- day31to60/day32.py
+++ day31to60/day32.py
@@ -25,3 +25,24 @@ print(timetable[1])
 print(timetable[2])
 print(timetable[3])
 print(timetable[4])
+
+#Lists & Loops:
+timetable = ["Computer Science", "Math", "English", "Art", "Watch TV"]
+for lesson in timetable:
+  print(lesson)
+
+tablenew = ["a", "b", "c", "d", "e"]
+for char in tablenew:
+  print(char)
+
+
+#Common Errors:
+#Remember, start counting from 0. The first color in the list, red, is index 0. not 1
+colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
+print(f"The first color is {colors[0]}")
+
+#2: not 6 -> 5
+#There is no index 6 because you need to start counting from 0. The 6th item in the list,
+#violet, is index 5.
+colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
+print(f"The first color is {colors[5]}")
