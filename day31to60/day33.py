216909486d686ad7bdaf2e6ae70755de738549a0
magda <magdamoraga14@gmail.com>
2025-09-03 21:32:02 +0200

    day 33 - dynamic list, subroutine, append

--- day31to60/day33.py
+++ day31to60/day33.py
@@ -1,2 +1,21 @@
 #Day 33 - DYNAMIC LISTS
 # add & remove istems as we go
+
+#computer builds a list with nothing in it
+myAgenda = []
+
+def printList():
+  print()
+  for item in myAgenda:
+    print(item)
+  print()
+
+while True:
+  item = input("What's next on your agenda? >")
+  myAgenda.append(item)
+  stop = input("do you want to add action? >")
+  if stop == "no" or stop == "No":
+    break
+  printList()
+
+print(myAgenda)
