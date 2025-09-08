4140fb48736ac085ac2a6591ed47402913b0a0ec
magda <magdamoraga14@gmail.com>
2025-09-08 23:02:37 +0200

    day 34 - added alternative solution. Code moved to a sub.

--- day31to60/day34.py
+++ day31to60/day34.py
@@ -1,34 +1,34 @@
 #Day 34 - Iterating through lists with loops:
-#PRETTY PRINT:
-import os, time
-listOfEmail = []
+# #PRETTY PRINT:
+# import os, time
+# listOfEmail = []
 
-def prettyPrint():
-  os.system("clear") # start by clearing the screen
-  print("listofEmail") # print the title of my program
-  print() # print a blank line
-#   counter = 1
-#   for email in listOfEmail: # use for loop to access list
-#     print(f"{counter}: {email}")
-#     counter +=1 
-for index in range(len(listOfEmail)): # len counts how many items in a list
-    print(f"{index}: {listOfEmail[index]}") 
-time.sleep(1)
+# def prettyPrint():
+#   os.system("clear") # start by clearing the screen
+#   print("listofEmail") # print the title of my program
+#   print() # print a blank line
+# #   counter = 1
+# #   for email in listOfEmail: # use for loop to access list
+# #     print(f"{counter}: {email}")
+# #     counter +=1 
+# for index in range(len(listOfEmail)): # len counts how many items in a list
+#     print(f"{index}: {listOfEmail[index]}") 
+# time.sleep(1)
 
-while True:
-  print("SPAMMER Inc.")
-  menu = input("1. Add email\n2: Remove email\n3. Show emails\n4. Get SPAMMING\n> ")
-  if menu == "1":
-    email = input("Email > ")
-    listOfEmail.append(email)
-  elif menu =="2":
-    email = input ("Email > ")
-    if email in listOfEmail:
-      listOfEmail.remove(email)
-    elif menu == "3": 
-        prettyPrint() 
-  time.sleep(1)
-  os.system("clear")
+# while True:
+#   print("SPAMMER Inc.")
+#   menu = input("1. Add email\n2: Remove email\n3. Show emails\n4. Get SPAMMING\n> ")
+#   if menu == "1":
+#     email = input("Email > ")
+#     listOfEmail.append(email)
+#   elif menu =="2":
+#     email = input ("Email > ")
+#     if email in listOfEmail:
+#       listOfEmail.remove(email)
+#     elif menu == "3": 
+#         prettyPrint() 
+#   time.sleep(1)
+#   os.system("clear")
   
   #2---------------------------
   #common errors:
@@ -75,6 +75,17 @@ def prettyPrint():
     counter += 1 
   time.sleep(1)
   
+  #Alternative sollution:
+def spam(max):
+    for i in range(0,max):
+        print(f"""Email {i+1}
+            Dear {listOfEmail[i]}
+            It has come to our attention that you're missing out on the amazing Replit 100 days of code. We insist you do it right away. If you don't we will pass on your email address to every spammer we've ever encountered and also sign you up to the My Little Pony newsletter, because that's neat. We might just do that anyway.
+            Love and hugs,
+            Ian Spammington III""")
+        time.sleep(1)
+        os.system("clear")
+    
 while True:
   print("Yummy Food Restaurant")
   menu = input("1. Order food\n2: Delete order\n3. Leave a review\n4. Delivery\n> ")
@@ -88,6 +99,9 @@ while True:
   elif menu == "3": 
     prettyPrint()
   elif menu == "4":
+    #Alternative solution: 
+    spam(10)
+      #My solution
     counter = 1
     for index in range(10):
       print(f"Email {counter}:")
