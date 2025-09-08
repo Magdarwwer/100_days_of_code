b878fc82a1efbe8c11a90ad7f8464267701b9d27
magda <magdamoraga14@gmail.com>
2025-09-08 23:57:45 +0200

    day 35 - added funtion

--- day31to60/day35.py
+++ day31to60/day35.py
@@ -2,5 +2,12 @@
 import os, time
 listOfTasks = []
 
+def checkIfexistsandAdd(task):
+  if task in listOfTasks:
+    print("item already exists in a list")
+  else:
+    listOfTasks.append(addedTask)
+#menu - add, change, remove
+
 while True:
     menu = input("")
