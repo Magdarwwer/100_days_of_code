2f15d6d86ad1b0113907d44707e41a830934bc71
magda <magdamoraga14@gmail.com>
2025-09-09 20:23:35 +0200

    day 36 - using .lower(), .upper(), .title(), .capitalize()

--- day31to60/day36.py
+++ day31to60/day36.py
@@ -1,1 +1,8 @@
 #Day 36 
+name = input("What's your name? : ")
+if name.lower().strip() == "david" or name.lower() == "magda":
+  print(f"Hello {name}")
+else:
+  print("I don't know you")
+
+#.strip() - removes spaces
