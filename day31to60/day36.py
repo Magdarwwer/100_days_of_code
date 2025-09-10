674c389a497f5053757c56dcab0ab124a44753a2
magda <magdamoraga14@gmail.com>
2025-09-10 13:48:35 +0200

    day 36 - title, upper, capitalzie

--- day31to60/day36.py
+++ day31to60/day36.py
@@ -4,5 +4,10 @@ if name.lower().strip() == "david" or name.lower() == "magda":
   print(f"Hello {name}")
 else:
   print("I don't know you")
+  
+ourTitle = "this is our title "
+print(f"using .title() :{ourTitle.title()}")
+print(f"using .upper() :{ourTitle.upper()}")
+print(f"using capitalize(): {ourTitle.capitalize()}")
 
 #.strip() - removes spaces
