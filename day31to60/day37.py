32dd92c5122adb362533a7852b91321e6ad791c5
magda <magdamoraga14@gmail.com>
2025-09-10 20:40:10 +0200

    day 37 - chopping strings

---
+++ day31to60/day37.py
@@ -0,0 +1,10 @@
+#Day 37 - string slicing
+# chopping text into parts 
+
+#1)This code outputs the 'H' from 'Hello'
+myString = "Hello there my friend."
+print(myString[0])
+print(myString[0:5])
+print(myString[:11])
+print(myString[6:11])
+print(myString[12:0])
