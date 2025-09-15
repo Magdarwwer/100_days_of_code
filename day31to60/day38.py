d2adbb290ca0a285b3342fafa7faf6ebe330292c
magda <magdamoraga14@gmail.com>
2025-09-15 22:36:50 +0200

    day 38 - added vowels, letter in vowels list color yellow, added end ='' to displaying a letter

--- day31to60/day38.py
+++ day31to60/day38.py
@@ -1,13 +1,13 @@
 #Day 38 - combine Strings & Loops 
 #strings are lists in disguise
-
+vowels = ["a", "e", "i", "o", "u"]
 #a lot of
 myString = "Day 38"
 myString = input("Type something> ")
 for letter in myString:
-  if letter.lower() == "a":
+  if letter.lower() in vowels:#== "a":
     print('\033[33m', end='')
-print(letter)
+print(letter, end = '')
 print('\33[0m', end = '')
 
 # This code outputs:
