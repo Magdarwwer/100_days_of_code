7e807e3946921f2830cc041b40629f6ac282a52f
magda <magdamoraga14@gmail.com>
2025-09-15 22:13:31 +0200

    day 38 - start, strings as lists

---
+++ day31to60/day38.py
@@ -0,0 +1,17 @@
+#Day 38 - combine Strings & Loops 
+#strings are lists in disguise
+
+#a lot of
+myString = "Day 38"
+myString = input("Type something> ")
+for letter in myString:
+  print(letter)
+
+# This code outputs:
+#D
+#a
+#y
+
+#3
+#8
+# this is a comment in the code, the computer will ignore it
