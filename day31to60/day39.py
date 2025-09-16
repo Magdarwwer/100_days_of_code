ca6416b57b5b6870b5bdecce307522b249a2d549
magda <magdamoraga14@gmail.com>
2025-09-16 22:26:21 +0200

    day 39 - hangman daily challenge; added sub for rremoving letters wip. Added alphabet to check used up letters (removing letters in the process and checking if they're in alphabet -> if no letter is used), added while True loop with main code WIP

--- day31to60/day39.py
+++ day31to60/day39.py
@@ -10,3 +10,36 @@ print(wordChosen)
 letter = input("Pick a letter> ")
 
 #check if just one char len(letter) == 1
+
+
+#First the word is picked
+alphabent = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "w", "v", "y", "z"]
+def remove_letter(letter, list):
+  for item in list:
+      if item == letter:
+        list.remove(letter)
+        
+life_counter = 5
+#Then the user starts to guess letters
+while True:
+  letter = input("Pick a letter> ")
+  if len(letter) == 1:
+    if letter in alphabent:
+      if letter in wordChosen:
+        for char in wordChosen:
+          if char == letter:
+            print(letter, end ='')
+          else:
+            print("_", end ='')
+      else:
+        print("Nope, not in there.")
+        print(f"{life_counter} left")
+        life_counter =-1
+    else:
+      print("Letter already used")
+      #continue
+  else:
+    print("Put in a letter man")
+    #continue
+    
+#Track what letters they used
