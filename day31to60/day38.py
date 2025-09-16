6f171a9a3fad8d60d18b6bbef17e73a9ebaf6725
magda <magdamoraga14@gmail.com>
2025-09-16 07:01:50 +0200

    day 38 - FOR LOOP for coloring letters added

--- day31to60/day38.py
+++ day31to60/day38.py
@@ -18,3 +18,28 @@ print('\33[0m', end = '')
 #3
 #8
 # this is a comment in the code, the computer will ignore it
+
+#Daily challenge 38:
+user_input = input("\n'\033[33m'Write a sentence > '\33[0m'")
+
+print("FOR:")
+for letter in user_input:
+  if letter.lower() == "r":
+    print('\033[31m', end="")
+  elif letter.lower() == "b":
+    print('\033[34m', end="")
+  elif letter.lower() == "g":
+    print('\033[92m', end="")
+  elif letter.lower() == "y":
+    print('\033[93m', end="")
+  elif letter.lower() == "p":
+    print('\033[35m', end="")
+  elif letter.lower() == "o":
+    print('\033[33m', end="")
+  elif letter == " ":
+    print('\33[0m', end = '')
+  elif letter.lower() == "m":
+    print('\33[95m', end = '')
+  else:
+    print('\33[0m', end = '')
+  print(letter, end ='')
