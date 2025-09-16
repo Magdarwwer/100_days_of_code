65f576ef3f4ff471591a990fa40cc1cc1c2cd089
magda <magdamoraga14@gmail.com>
2025-09-16 22:01:06 +0200

    day 39 - picking randdom words using random library.

--- day31to60/day39.py
+++ day31to60/day39.py
@@ -1,2 +1,12 @@
 #Day 39 - Hangman; string slicing, string manipulation:
 #This hangman project combines lists, string manipulation, and slicing, along with other concepts.
+import random
+
+listOfWords = ["british", "suave", "integrity", "accent", "evil", "genius", "Downton"]
+
+wordChosen = random.choice(listOfWords)
+print(wordChosen)
+
+letter = input("Pick a letter> ")
+
+#check if just one char len(letter) == 1
