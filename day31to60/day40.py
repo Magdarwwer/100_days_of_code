679e3e3981c90d4676d3d214abc69c693577a84d
magda <magdamoraga14@gmail.com>
2025-09-27 08:58:21 +0200

    day 40 - Dictionaries

--- day31to60/day40.py
+++ day31to60/day40.py
@@ -1,1 +1,7 @@
-#Day 40
+#Day 40- Dictionaries
+
+#lists are accesed by numeric numbers, not randomly -> no connection between 
+#value & key - ex. job <-> name .... DO YOU REMEMBER the name of assistant? -> John <- NO INDEXES
+myUser = {"name" : "David", "job": "bald king"}
+print(myUser["name"])
+print(myUser["job"])
