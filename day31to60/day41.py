77bd9f1154f971460d8c62278596831969a34a0b
magda <magdamoraga14@gmail.com>
2025-10-05 23:44:50 +0200

    day 41 - dictionaries & loops. Matrix try out from math AGH classes

---
+++ day31to60/day41.py
@@ -0,0 +1,7 @@
+#Day 41 - dictionaries & loops
+
+#dictionaries stores two bits of data - why two bits
+myDictionary = {"name" : "Ian", "health": 219, "strength": 199, "equipped": "Axe"}
+
+for i in myDictionary:
+  print(myDictionary[i])
---
+++ others/matrixmultiply.py
@@ -0,0 +1,10 @@
+#Czy zmiana kolejności indexów wpływa na długość:
+def iloczyn_skalarny():
+    print("aaa")
+    
+    
+    
+for item in matrix:
+    for jtem in matrix:
+        for ktem in matrix:
+            
