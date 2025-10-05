a7b745da2f15b7ce10c10f4a9e73a2f13c23fa4a
magda <magdamoraga14@gmail.com>
2025-10-05 23:46:41 +0200

    day 41 - printing keys' names

--- day31to60/day41.py
+++ day31to60/day41.py
@@ -5,3 +5,8 @@ myDictionary = {"name" : "Ian", "health": 219, "strength": 199, "equipped": "Axe
 
 for i in myDictionary:
   print(myDictionary[i])
+  
+print()
+print("KEYS:")
+for i in myDictionary:
+  print(i)
