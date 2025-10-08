a618316f5063ec8f411290b7373b255a54a32fdc
magda <magdamoraga14@gmail.com>
2025-10-08 15:07:54 +0200

    check if numbers are lower/higher

--- others/evenOdd.py
+++ others/evenOdd.py
@@ -2,3 +2,9 @@
 low_number = input("Give down range number:")
 high_number = input("Give up range number:")
 
+#check if numbers valid
+if low_number < high_number:
+    #OK - proceed
+    
+    #find the smallest even number in a range:
+    
