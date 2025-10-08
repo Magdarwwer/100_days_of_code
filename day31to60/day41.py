884c9aaa38de2086b37a69ab0dc343872f6ae283
magda <magdamoraga14@gmail.com>
2025-10-08 15:23:48 +0200

    day41 - fix my code challenge done

--- day31to60/day41.py
+++ day31to60/day41.py
@@ -43,3 +43,17 @@ myDictionary = {"name" : "Ian", "health": 219, "strength": 199, "equipped": "Axe
 #
 for name, value in myDictionary.items():
   print(f"{name}, {value}")
+  
+  
+  
+#FIX My CODE:
+myDictionary = {"name" : "David the Mildy Terrifying", "health": 186, "strength": 4, "equipped":"l33t haxx0r p0werz"}
+
+for name, value in myDictionary.items():
+  print(f"{name}:{value}")
+
+  if (name == "strength"):
+    if value > 100:
+      print("Whoa, SO STRONG")
+    else:
+      print("Looks like you skipped leg day, arm day, chest day and, well, gym day entirely bro!")
