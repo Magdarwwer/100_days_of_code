b84fd3a1c8f4060cf707b073fe3ba023cb3c07d7
magda <magdamoraga14@gmail.com>
2025-10-09 14:39:30 +0200

    day42 - added alternative solution

--- day31to60/day42.py
+++ day31to60/day42.py
@@ -36,3 +36,26 @@ for name, value in pokedict.items():
 print("\033[0m")
 
 
+#ALTERNATIVE SOLUTION: COMMENT OUT
+mokedex = {"Beast Name": None, "Type": None, "Special Move": None, "HP": None, "MP": None}
+
+print("MokéBeast")
+print()
+
+for name, value in mokedex.items():
+  mokedex[name] = input(f"{name}:\t").strip().title()
+
+if mokedex["Type"]=="Earth":
+  print("\033[32m", end="")
+elif mokedex["Type"]=="Air":
+  print("\033[37m", end="")
+elif mokedex["Type"]=="Fire":
+  print("\033[31m", end="")
+elif mokedex["Type"]=="Water":
+  print("\033[34m", end="")
+else:
+  print("\033[33m", end="")
+
+for name, value in mokedex.items():
+  print(f"{name:<15}: {value}")
+
