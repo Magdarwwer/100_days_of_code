8768d6683521e1d51bc0cbd2220eccd1c7e5e179
magda <magdamoraga14@gmail.com>
2025-10-09 12:30:33 +0200

    day42 - added code

---
+++ day31to60/day42.py
@@ -0,0 +1,28 @@
+#Day42 - 
+pokemonTypes = ["fire", "water", "earth", "wind", "magical", "szkorpon"]
+pokedict = {"name": None, "type": None, "special move": None, "HP": None, "MP":None }
+
+def checkPokeType(ptype):
+  if ptype in pokemonTypes:
+    if ptype == "fire":
+      print("\033[31m", end="")
+    elif ptype == "water":
+      print("\033[34m", end="")
+    elif ptype == "earth":
+      print("\033[32m", end="")
+    elif ptype == "wind":
+      print("\033[37m", end="")
+    elif ptype == "magical":
+      print("\033[35m", end="")
+    elif ptype == "szkorpon":
+      print("\033[36m", end="")
+    else:
+      print("\033[37m", end="")
+
+for name in pokedict.keys():
+  pokedict[name] = input(f"Input your beast's {name} > ")
+
+print("--------")
+for name, value in pokedict.items():
+  print(f"{checkPokeType(pokemonTypes["type"]}{name}:{value}")
+
