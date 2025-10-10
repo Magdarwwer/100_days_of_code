2f0d9c3a1482a50bb54c60ec779ba5013c6b7bcc
magda <magdamoraga14@gmail.com>
2025-10-10 06:07:56 +0200

    day43 - file and initial code created

--- day31to60/day43.py
+++ day31to60/day43.py
@@ -1,1 +1,14 @@
 #Day 43 - Tables/ 2D DIMENTIONAL LISTS
+#ertical data is used for fields: one category - name, ID, favorite biscuit, etc
+#Behind the scenes, we see a list inside a list
+my1DList = ["Johnny", 21, "Mac"]
+
+my2DList = [ ["Johnny", 21, "Mac"],
+             ["Sian", 19, "PC"],
+             ["Gethin", 17, "PC"] ]
+
+my2DList = [ ["Johnny", 21, "Mac"], ["Sian", 19, "PC"], ["Gethin", 17, "PC"] ]
+
+print(my2DList)
+
+# This will output [['Johnny', 21, 'Mac'], ['Sian', 19, 'PC'], ['Gethin', 17, 'PC']]
